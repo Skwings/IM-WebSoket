a4a51d3622cf8c53e620fea28269ec82ff8f8fa6
skwings <1347609251@qq.com>
2021-01-14 23:20:21 +0800

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,90 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="logOut" class="logOutBtn">注销</button>
+    <p>{{this.username}}，欢迎您来到Skwings聊天室</p>
+
+    <input type="text" placeholder="请输入消息" id="" v-model="msg" @keyup.enter="handleSendBtnClick">
+    <button @click="handleSendBtnClick">发送</button>
+    <ul>
+      <li v-for="item of msgList" :key="item.id">
+        <p>
+          <span>{{new Date(item.dateTime)}}</span>  
+        </p>
+        <p>
+          <span>{{item.username}}:</span>
+          <span>{{item.msg}}</span>
+        </p>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+const ws = new WebSocket('ws://localhost:8000');
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      msg: '',
+      username: '', //用户名
+      msgList: [] //消息列表
+    }
+  },
+  methods: {
+    handleSendBtnClick(){ //发送消息
+      const msg = this.msg;
+      if(!msg.trim().length){
+        return
+      }
+      ws.send(JSON.stringify({
+        id: new Date(),
+        username: this.username,
+        dateTime: new Date().getTime(),
+        msg: this.msg
+      }))
+    },
+    logOut(){ //注销
+      localStorage.removeItem('IMUsername');
+      this.$router.push('/login')
+    },
+
+    handleWsOpen(e){
+      console.log('FE: Websocket open', e)
+    },
+    handleWsClose(e){
+      console.log('FE: Websocket close', e)
+    },
+    handleWsError(e){
+      console.log('FE: Websocket error', e)
+    },
+    handleWsMsg(e){
+      console.log('FE: Websocket message', e)
+      this.msgList.push(JSON.parse(e.data) );
+    }
+  },
+  mounted() {
+    this.username = localStorage.getItem('IMUsername')
+
+    if(!this.username){
+      this.$router.push('/login');
+      return
+    }
+
+    ws.addEventListener('open', this.handleWsOpen.bind(this) , false)
+    ws.addEventListener('close', this.handleWsClose.bind(this), false)
+    ws.addEventListener('error', this.handleWsError.bind(this), false)
+    ws.addEventListener('message', this.handleWsMsg.bind(this), false)
+  },
 }
 </script>
+
+<style lang="scss" scoped>
+.home{
+  position: relative;
+  .logOutBtn{
+    cursor: pointer;
+  }
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="Login">
+    <input type="text"  id="" placeholder="请输入您要使用的用户名" v-model="username">
+    <span>{{this.usernameTip}}</span>
+    <button @click="setUsername">进入聊天室</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      username: '',
+      usernameTip: ''
+    }
+  },
+  methods: {
+    setUsername(){
+      const username = this.username.trim()
+      if(username.length > 6){
+        usernameTip = '账号名称长度不得大于六位数字！'
+      }else if(username == ''){
+        this.usernameTip = '账号名称不能为空'
+      }
+
+      localStorage.setItem('IMUsername', username);
+      this.$router.push('/home')
+    }
+  },
+  mounted() {
+    const username = localStorage.getItem('IMUsername')
+    if(username){
+      this.$router.push('/home')
+    }
+  },
+}
+</script>
